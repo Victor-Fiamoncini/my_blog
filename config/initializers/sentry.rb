48ece7992aa0169e8ccc6b7f3d678fb22068c886
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2026-06-26 10:39:02 -0300

    chore: add sentry_dsn to rails encrypted credentials

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Sentry.init do |config|
-  config.dsn = ENV["SENTRY_DSN"]
+  config.dsn = Rails.application.credentials.sentry_dsn
   config.breadcrumbs_logger = [ :active_support_logger, :http_logger ]
 
   # Add data like request headers and IP for users,
